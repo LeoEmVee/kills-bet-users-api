47f879f78f55abe5b1be756e8e8cebea92798f97
LeoEmVee <santito79@hotmail.com>
2022-04-07 14:58:36 +0200

    Add @nestjs/config to enable process.env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.SERVER_PORT;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
 }
 bootstrap();
